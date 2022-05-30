baa5c07af0647f470629a3b97b5e9a3566ed3002
Artem Teslia <eSquire.github.repos@gmail.com>
2022-05-30 11:02:51 +0300

    Add Main.php
    Add some PHP and HTML code

---
+++ Lection 19/Main.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercise №1</title>
+    </head>
+    <body>
+
+        <?php
+        
+$ sum = $_GET['firstPair'] + $_GET['secondPair'];
+echo "$sum";
+
+        ?>
+
+    </body>
+</html>
