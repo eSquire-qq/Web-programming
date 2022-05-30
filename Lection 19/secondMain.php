7c4ef9f5310c12ab65f77e1583e95152cd5f6300
Artem Teslia <eSquire.github.repos@gmail.com>
2022-05-30 11:08:30 +0300

    Add secondMain.php

---
+++ Lection 19/secondMain.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercise №2</title>
+    </head>
+    <body>
+
+        <?php
+        
+$firstNum = 2;
+$firstNum.=($secondNum = ('Hello !'.$firstNum);
+$firstNum += $secondNum + $secondNum;
+
+        ?>
+
+    </body>
+</html>
