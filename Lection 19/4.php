8872516152f1eb7e352d26f9b8c1211f8f4e1a2f
Artem Teslia <eSquire.github.repos@gmail.com>
2022-05-30 11:17:33 +0300

    Update 4.php

---
+++ Lection 19/4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercise №4</title>
+    </head>
+    <body>
+
+        <?php
+        
+$x1 = 3;
+$y1 = 7;
+
+$x2 = 8;
+$y2 = 9;
+
+echo "A($x1,$y1) B($x2,$y2)";
+$equal = sqrt(pow(($x1 - $x2) + pow($y1 - $y2),2));
+echo "\nAB = $equal";
+
+        ?>
+
+    </body>
+</html>
