d730d6cd1bb2e131cc1fd23a370357b03d034914
Artem Teslia <eSquire.github.repos@gmail.com>
2022-05-30 11:11:45 +0300

    Create 3.php

---
+++ Lection 19/3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Exercise №1</title>
+    </head>
+    <body>
+
+        <?php
+        
+$num = 3;
+$equal = pow(pow($num,7) + pow(7,$num),(1/7));
+echo "$equal";
+
+        ?>
+
+    </body>
+</html>
